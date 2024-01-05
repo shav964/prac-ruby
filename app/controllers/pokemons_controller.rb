b2c55d840fd376c45b40162c7249f534a062a1ce
shav964 <ariados0618@gmail.com>
2024-01-05 11:54:48 +0900

    祝・勝利数リセット

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,6 +3,7 @@ class PokemonsController < ApplicationController
 
   def index
     @pokemon = Pokemon.first
+    puts @pokemon.inspect
     @pokemons = Pokemon.all
     render :index, locals: { pokemon: @pokemon, pokemons: @pokemons }
   end
@@ -14,7 +15,8 @@ class PokemonsController < ApplicationController
     @pokemons = Pokemon.all
     @additional_wins = 0
     @pokemon = Pokemon.find_by(pokedex_number: params[:pokedex_number])
-  
+    logger.debug("DEBUG: @pokemon in update_wins = #{@pokemon.inspect}")
+
     if @pokemon.nil?
       # @pokemon が見つからなかった場合の処理（例: リダイレクト、エラーメッセージの表示など）
       # ここでは一時的に例外を発生させていますが、本番環境では適切なエラーハンドリングが必要です。
@@ -24,7 +26,7 @@ class PokemonsController < ApplicationController
 
   def update_wins
     @pokemon = Pokemon.find_by(pokedex_number: pokemon_params[:pokedex_number])
-    additional_wins = pokemon_params[:wins].to_i
+    additional_wins = pokemon_params[:wins].to_i || 0
 
     if @pokemon.update(wins: @pokemon.wins + additional_wins)
       redirect_to update_wins_page_pokemon_path(@pokemon), notice: "#{additional_wins}勝利数が追加されました！"
